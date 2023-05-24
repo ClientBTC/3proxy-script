92ced8e19ceb654c7ecef9ee531d974ca83fd9f6
main-user <104539607+main-user@users.noreply.github.com>
2023-05-24 20:12:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo -i
 apt-get update
-apt-get install -y build-essential
+apt-get install gcc make git -y
 wget --no-check-certificate https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz -O 3proxy-0.9.4.tar.gz
 tar xzf 3proxy-0.9.4.tar.gz
 cd 3proxy-0.9.4
@@ -8,11 +8,11 @@ make -f Makefile.Linux
 cd src
 mkdir /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/main-user/3proxy-script/main/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://raw.githubusercontent.com/main-user/3proxy-script/main/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+wget --no-check-certificate  https://raw.githubusercontent.com/main-user/3proxy-script/main/3proxy
 chmod +x /etc/init.d/3proxy
 update-rc.d 3proxy defaults
